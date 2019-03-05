12a04e6565ae8c6ecab4b9abaeeb540ff581a1ee
nirvik00 <nirviksaha@gatech.edu>
2019-03-05 06:15:29 -0500

    final files code update

--- dbConnector.py
+++ dbConnector.py
@@ -9,9 +9,10 @@ def readNodeFile(filename):
         i=line.split(",")[0]
         x=line.split(",")[1]
         y=line.split(",")[2]
-        z=line.split(",")[3].split("\n")[0]
+        z=line.split(",")[3]
+        t=line.split(",")[4].split("\n")[0]
         #print(i,x,y,z)
-        node_li.append([i,x,y,z])
+        node_li.append([i,x,y,z,t])
     return node_li
 
 def readEdgeFile(filename):
@@ -24,9 +25,10 @@ def readEdgeFile(filename):
         z0=line.split(",")[3]
         x1=line.split(",")[4]
         y1=line.split(",")[5]
-        z1=line.split(",")[6].split("\n")[0]
+        z1=line.split(",")[6]
+        t=line.split(",")[7].split("\n")[0]
         #print(i,x0,y0,z0,x1,y1,z1)
-        edge_li.append([i,x0,y0,z0,x1,y1,z1])
+        edge_li.append([i,x0,y0,z0,x1,y1,z1,t])
     return edge_li
 
 
@@ -42,7 +44,8 @@ def writeNodesToDB(node_li):
             'nsId':i[0],
             'x':round(float(i[1])/1000,2),
             'y':round(float(i[2])/1000,2),
-            'z':round(float(i[3])/1000,2)
+            'z':round(float(i[3])/1000,2),
+            't':i[4]
             }
         result=ns_elements.insert_one(node_data)
         #print('One post: {0}'.format(result.inserted_id))
@@ -64,7 +67,8 @@ def writeEdgesToDB(edge_li):
             'z0':round(float(i[3])/1000,2),
             'x1':round(float(i[4])/1000,2),
             'y1':round(float(i[5])/1000,2),
-            'z1':round(float(i[6])/1000,2)
+            'z1':round(float(i[6])/1000,2),
+            't':i[7]
             }
         result=ns_elements.insert_one(edge_data)
         #print('One post: {0}'.format(result.inserted_id))        
@@ -73,6 +77,7 @@ NODE_LI=readNodeFile("nodes.dat")
 EDGE_LI=readEdgeFile("edges.dat")
 writeNodesToDB(NODE_LI)
 writeEdgesToDB(EDGE_LI)
+
 
 print("done")
 
---
+++ plugs_web_conversion.py
@@ -0,0 +1,118 @@
+import rhinoscriptsyntax as rs
+
+
+class Node(object):
+    def __init__(self, x,y,z,i,element_set):
+        self.x=x
+        self.y=y
+        self.z=0
+        self.id=i
+        self.pt=[x,y,0]
+        self.element_set=element_set
+
+class Edge(object):
+    def __init__(self,n0,n1,i,element_set):
+        self.node0=n0
+        self.node1=n1
+        self.id=i
+        self.element_set=element_set
+    
+def setCirculationGraph():
+    x=rs.ObjectsByLayer("ns_nodes")
+    node_li=[]
+    k=0
+    for i in x:
+        c=rs.CircleCenterPoint(i)
+        node_li.append(Node(c[0],c[1],0,k,"NCN"))
+        k+=1
+    x_gcn=rs.ObjectsByLayer("ns_gcn_nodes")
+    for i in x_gcn:
+        c=rs.CircleCenterPoint(i)
+        node_li.append(Node(c[0],c[1],0,k,"GCN"))
+        k+=1
+    y=rs.ObjectsByLayer("ns_edges")
+    edge_li=[]
+    k=0
+    for i in y:
+        p=rs.CurveStartPoint(i)
+        q=rs.CurveEndPoint(i)
+        node0=None
+        node1=None
+        sum0,sum1=0,0
+        for j in node_li:
+            r=j.pt
+            if(rs.Distance(r,p)<10):
+                node0=j
+                sum0+=1
+                break
+        for j in node_li:
+            r=j.pt            
+            if(rs.Distance(r,q)<10):
+                node1=j
+                sum1+=1
+                break
+        if(sum0>0 and sum1>0):
+            edge_li.append(Edge(node0,node1,k,"road"))
+        k+=1
+    y_green=rs.ObjectsByLayer("ns_green_edges")
+    for i in y_green:
+        p=rs.CurveStartPoint(i)
+        q=rs.CurveEndPoint(i)
+        node0=None
+        node1=None
+        sum0,sum1=0,0
+        for j in node_li:
+            r=j.pt
+            if(rs.Distance(r,p)<10):
+                node0=j
+                sum0+=1
+                break
+        for j in node_li:
+            r=j.pt            
+            if(rs.Distance(r,q)<10):
+                node1=j
+                sum1+=1
+                break
+        if(sum0>0 and sum1>0):
+            edge_li.append(Edge(node0,node1,k,"green"))
+    
+    for i in edge_li:
+        p=i.node0.pt
+        q=i.node1.pt
+        L=rs.AddLine(p,q)
+        rs.MoveObject(L,[0,0,1000])
+        
+    return [node_li,edge_li]
+
+
+def writeNodeToCsv(node_li, filename):
+    f=open(filename,"w")
+    for i in node_li:
+        s=str(i.id)+","+str(round(i.x,2))+","+str(round(i.y,2))+","+str(0)+","+i.element_set+"\n"
+        f.write(s)
+    f.close()
+
+def writeEdgeToCsv(edge_li, filename):
+    f=open(filename,"w")
+    s=""
+    for i in edge_li:
+        s=str(i.id)+","+str(i.node0.x)+","+str(i.node0.y)+","+str(0)+","+str(i.node1.x)+","+str(i.node1.y)+","+str(0)+","+i.element_set+"\n"
+        f.write(s)
+    f.close()
+
+
+def moveObject(name):
+    obj=rs.ObjectsByLayer(name)
+    b=rs.ObjectsByLayer("ns_bounding_box")[0]
+    
+    c=rs.CurveAreaCentroid(b)[0]
+    d=[-c[0],-c[1],-c[2]]
+    for i in obj:
+        rs.MoveObject(i,d)
+    
+#moveObject("kyojimas")
+GRAPH=setCirculationGraph()
+NODE_LI=GRAPH[0]
+EDGE_LI=GRAPH[1]
+writeNodeToCsv(NODE_LI,"nodes.dat")
+writeEdgeToCsv(EDGE_LI,"edges.dat")
